252d34fc72a94619da0a2498cdbf54d27ba9438e
avosalmon <avosalmon@gmail.com>
2019-08-30 08:40:57 +0200

    upload assets to S3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const s3Plugin = require('webpack-s3-plugin');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,3 +14,23 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new s3Plugin({
+                include: /.*\.(css|js)$/,
+                s3Options: {
+                    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                    region: process.env.AWS_DEFAULT_REGION,
+                },
+                s3UploadOptions: {
+                    Bucket: process.env.AWS_BUCKET,
+                    CacheControl: 'public, max-age=31536000'
+                },
+                directory: 'public'
+            })
+        ]
+    });
+}
